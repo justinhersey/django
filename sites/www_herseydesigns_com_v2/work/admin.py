cc01d381d0d0f12fa8df6000e017586da54b115e
Justin Hersey <justin.hersey@gmail.com>
2014-08-24 07:16:48 -0700

    commit

--- sites/www_herseydesigns_com_v2/work/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from books.models import Book, Person
-from Django.contrib import admin
-admin.site.register (Book)
-admin.site.register (Person)
